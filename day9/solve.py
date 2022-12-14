b2de5a1bfdd8a34701ab0cdb5bda711494262dcc
Mike Conroy <mconroy@mulesoft.com>
2022-12-14 19:57:23 +0000

    Day 9 - Part 2

--- day9/solve.py
+++ day9/solve.py
@@ -5,7 +5,8 @@ for line in f.readlines():
     line = line.strip()
     instructions.append(line)
 
-def processInstruction(instruction, head, tail, visited):
+# Updated to accept an array of tails for Part 2
+def processInstruction(instruction, head, tails, visited):
     direction, steps = instruction.split(" ")
     steps = int(steps)
 
@@ -19,10 +20,17 @@ def processInstruction(instruction, head, tail, visited):
             head = (head[0], head[1] + 1)
         elif direction == "D":
             head = (head[0], head[1] - 1)
-        tail = moveTail(head, tail)
-        visited.add(tail)
-        # print("HEAD", head, "TAIL", tail)
-    return head, tail
+        # Process the first tail
+        tails[0] = moveTail(head, tails[0])
+        # visited.add(tails[0])
+
+        # Process the remaining tails in the array
+        for index, tail in enumerate(tails):
+            if index == 0:
+                continue
+            tails[index] = moveTail(tails[index - 1], tails[index])
+        visited.add(tails[len(tails) - 1])
+    return head, tails
 
 # Check whether we need to move tail or not
 def isTailOneStepFromHead(head, tail):
@@ -58,14 +66,36 @@ def moveTail(head, tail):
 def part1():
     visited = set()
     head = (0,0)
-    tail = (0,0)
-    visited.add(tail)
+    tail = [(0,0)]
     for instruction in instructions:
         head, tail = processInstruction(instruction, head, tail, visited)
     return len(visited)
 
+def printGrid(head, tails):
+    for y in range(head[0] - 20, head[0] + 20):
+        for x in range(head[1] - 20, head[1] + 20):
+            printed = False
+            if head == (x,y):
+                print("H", end="")
+            else:
+                for index, tail in enumerate(tails):
+                    if tail == (x, y):
+                        printed = True
+                        print(index, end="")
+                        break
+            if not printed:
+                print(".",end="")
+        print("")
+    print("")
+
 def part2():
-    return 0
+    visited = set()
+    head = (0,0)
+    tails = [(0,0) for x in range(9)]
+    for instruction in instructions:
+        head, tails = processInstruction(instruction, head, tails, visited)
+        # printGrid(head, tails)
+    return len(visited)
 
 print("PART 1:", part1())
 print("PART 2:", part2())

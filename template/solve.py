03a646d7f6f54ed7928ce0608b1470dd79049361
Mike Conroy <mconroy@mulesoft.com>
2022-12-19 19:28:10 +0000

    Add template.

---
+++ template/solve.py
@@ -0,0 +1,16 @@
+import copy
+f = open("input", "r")
+
+inputArray = []
+for line in f.readlines():
+    line = line.strip()
+    inputArray.append(line)
+    
+def part1():
+    return 0
+
+def part2():
+    return 0
+    
+print("PART 1:", part1())
+print("PART 2:", part2())

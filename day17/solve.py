c70df6c2f8a4f173d7fa09a1e6da871418864012
Mike Conroy <mconroy@mulesoft.com>
2023-01-22 21:19:33 +0000

    Attempt Day 17 - Part 2

--- day17/solve.py
+++ day17/solve.py
@@ -85,6 +85,15 @@ def print_grid(settled_rocks):
                 print('.', end='')
         print()
 
+def get_new_heights(last_rock_settled, current_height, current_heights=[]):
+    for point in last_rock_settled:
+        if point[1] > current_height:
+            current_height = point[1]
+        # Only used in Part 2:
+        if current_heights and current_heights[point[0]] < point[1]:
+            current_heights[point[0]] = point[1]
+    return current_height, current_heights
+
 def part1(target):
 
     num_jets = len(jets)
@@ -111,11 +120,42 @@ def part1(target):
             jet_index += 1
 
         settled_rocks.update(rock)
-        current_height = get_height(settled_rocks)
+        current_height = get_new_heights(rock, current_height)[0]
         rocks_dropped += 1
-        if rocks_dropped % 100_000 == 0:
-            print(rocks_dropped, current_height)
+
     return current_height
+
+def remove_points_below_floor(settled_rocks):
+    heights = [0 for x in range(0,7)]
+    new_points = dict()
+    for point in settled_rocks:
+        if point[1] >= heights[point[0]]:
+            heights[point[0]] = point[1]
+            new_points[point[0]] = point
+    
+    return set(new_points.values())
+
+def settle_rock(rock, settled_rocks):
+    # TODO: THIS IS BROKEN - OR some part of the code after is broken.
+    # Trying to just track the highest points in each column.
+    # Any point below one of these points shouldn't be required.
+    # This is likely failing as column 0 could be height 0 whilst column 1 height 100.
+    # Allowing Rocks to slip through gaps.
+    settled_rocks.update(rock)
+    heights = [0 for x in range(0,7)]
+    new_points = dict()
+    for point in settled_rocks:
+        if point[1] >= heights[point[0]]:
+            heights[point[0]] = point[1]
+            new_points[point[0]] = point
+
+    return set(new_points.values())
+
+def get_relative_heights(current_heights, current_height):
+    relative_heights = []
+    for height in current_heights:
+        relative_heights.append(height - current_height)
+    return tuple(relative_heights)
 
 # Repeat part 1 but for 1_000_000_000_000 Rocks...
 # Ideas -
@@ -123,8 +163,88 @@ def part1(target):
 #   Just store the heighest point in each 'column'.
 #   This would reduce the number of checks required on each drop (only check 7 points). 
 #   Can't loop around 1 Trillion times...
+
+# Does the cycle ever repeat?
+#   If we hit a point where the jet_index & shape have been seen before then
+#   We can take that current height and multiply the cycle n times as it would reach the same height?
+#   ... The floor would also need to be the same pattern
+# Track the current floor Pattern?? For the state & cycle check?
+# Current floor y_values=[0,0,0,0,0,0,0]
+# Always keep them relative to column 0?
 def part2():
-    return 0
+    target = 1_000_000_000_000
+    num_jets = len(jets)
+    rocks_dropped = 0
+    jet_index = 0
+    current_height = 0
+    # Set of settled Rocks - starting with the floor:
+    settled_rocks = {(0,0), (1,0), (2,0),(3,0),(4,0),(5,0),(6,0)}
+
+    # An array where each element represents a column.
+    # Column 0 will always be 0 and the rest relative to that column.
+    current_heights = [0, 0, 0, 0, 0, 0, 0]
+    cached_states = dict()
+    cycle_hit = False
+    cycle_length = 0
+    cycle_height = 0
+    height_added_in_cycle = 0
+    height_at_cycle = 0
+    while rocks_dropped < target:
+        # Rocks Dropped increases by 1 for every new rock.
+        # Mod 5 gives us the appropriate shape to spawn.
+        rock_type = rocks[rocks_dropped % 5]
+
+        # Create a new rock so we don't modify the shapes array
+        rock = create_falling_rock(rock_type, current_height)
+
+        # Also need to check the floor pattern.
+        relative_heights = get_relative_heights(current_heights, current_height)
+        state = (rocks_dropped % 5, jet_index % num_jets, relative_heights)
+        # print(relative_heights)
+        if state in cached_states and cycle_hit == False:
+            print("Cycle Hit.")
+            print("Rocks Dropped:", rocks_dropped)
+            print("State Hit (rock_type, jet_index, relative_heights):", state)
+            print("Cached State (height, rocks_dropped):", cached_states[state])
+            cycle_hit = True
+            # Amount of height added in the cycle
+            cycle_height = current_height - cached_states[state][0]
+            # Number of rocks dropped in the cycle
+            cycle_length = rocks_dropped - cached_states[state][1]
+
+            print(target,"-", cached_states[state][1], "/", cycle_length)
+            times_to_repeat_cycle = (target - cycle_length) // cycle_length
+            print(times_to_repeat_cycle)
+            # Used to calculate the delta to reach 1_000_000_000_000 after processing.
+            height_at_cycle = current_height
+            height_added_in_cycle = cycle_height * times_to_repeat_cycle
+            rocks_dropped = cached_states[state][1] + (cycle_length * times_to_repeat_cycle)
+            print("Rocks Dropped:", rocks_dropped)
+            print("Rocks Dropped in Cycle:", cycle_length)
+            print("Height Added During Cycle:", cycle_height)
+            next
+        else:
+            cached_states[state] = (current_height, rocks_dropped)
+
+        # Shift Rock in the appropriate direction based on the jet
+        able_to_drop = True
+        while able_to_drop:
+            jet = jets[jet_index % num_jets]
+            rock = shift_rock(rock, jet, settled_rocks)
+            rock, able_to_drop = drop_rock(rock, settled_rocks)
+            jet_index += 1
+
+        settled_rocks.update(rock)
+        current_height, current_heights = get_new_heights(rock, current_height, current_heights)
+        # settled_rocks = settle_rock(rock, settled_rocks)
+        # current_height = get_height(settled_rocks)
+        rocks_dropped += 1
+
+    current_height += height_added_in_cycle
+    print(current_height)
+    current_height -= (height_at_cycle - cycle_height)
+    return current_height
     
 print("PART 1:", part1(2022))
+# 1597714288343 - Too high
 print("PART 2:", part2())

d6bddfd5b9e58e4830f9a0dae9cb38d03f4c3b56
Mike Conroy <mconroy@mulesoft.com>
2023-02-04 15:55:00 +0000

    Day 21 - Part 2

--- day21/solve.py
+++ day21/solve.py
@@ -1,3 +1,5 @@
+import copy
+
 f = open("input", "r")
 
 inputArray = []
@@ -41,8 +43,39 @@ def part1():
     vals, ops = parse_input()
     return get_monkey_value("root", vals, ops)
 
-def part2():
-    return 0
+def calc_new_yell(current, too_high, min, max):
+    if too_high:
+        max = current
+    else:
+        min = current
     
+    new_yell = (min + max) // 2
+    return new_yell, min, max
+
+def part2():
+    vals, ops = parse_input()
+    root_lhs = ops['root']['lhs']
+    root_rhs = ops['root']['rhs']
+    ops.pop("root")
+    min = 0
+    # max = 3_876_907_167_499
+    max = 5_000_000_000
+    yell = max // 2
+    root_lhs_value = -1
+    root_rhs_value = -2
+    while True:
+        vals["humn"] = yell
+        new_vals = copy.deepcopy(vals)
+        new_ops = copy.deepcopy(ops)
+        root_lhs_value = get_monkey_value(root_lhs, new_vals, new_ops)
+        root_rhs_value = get_monkey_value(root_rhs, new_vals, new_ops)
+        if root_lhs_value == root_rhs_value:
+            return yell
+        too_high = True if root_lhs_value > root_rhs_value else False
+        yell, min, max = calc_new_yell(yell, too_high, min, max)
+
 print("PART 1:", part1())
+# 3876907167501 is too high
+# 3876907167499 is too high
+# 3876907167495 is the right answer
 print("PART 2:", part2())
